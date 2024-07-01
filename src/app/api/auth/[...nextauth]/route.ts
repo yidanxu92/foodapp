7d91d4d785d07bee3473128a97bcee641880b79b
Yidan Xu <112134918+yidanxu92@users.noreply.github.com>
2024-07-01 19:05:09 -0400

    took out admin

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -80,14 +80,7 @@ export const authOptions = {
   }
 }
 
-export async function isAdmin() {
-  const session =await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) return false;
-  const user = await User.findOne({ email: userEmail });
-  if (!user) return false;
-  return user.isAdmin;
-}
+
 
 const handler = NextAuth(authOptions)
 
